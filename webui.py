41088fdf539b2931ab5ac6820e5fbf79c324d357
Jixian Wang <hellojixian@gmail.com>
2023-07-10 15:36:30 +0300

    update

--- webui.py
+++ webui.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import os
+import os, sys
 import gradio as gr
 from sdxl import generate_image
 from dotenv import load_dotenv
@@ -9,15 +9,17 @@ SERVER_PORT = os.environ.get("SERVER_PORT", '8000')
 SERVER_HOST = os.environ.get("SERVER_HOST", 'localhost')
 
 def generate_task(input_text):
-    # Generate HTML code dynamically based on the input
+  # Generate HTML code dynamically based on the input
   image = generate_image(input_text)
-  html_code = f"<h2>Generated Image:</h2>"
-  html_code += f"<p><img src='{image}'/></p>"
+  html_code = f"<h2>Prompts:</h2>"
+  html_code += f"<p>{input_text}</p>"
+  html_code += f"<h2>Generated Image:</h2>"
+  html_code += f"<p><img src='file/{image}'/></p>"
   return html_code
 
 webapp = gr.Interface(
-  title="Generate Image from Text",
-  inputs=[gr.components.Textbox(lines=20,
+  title="Generate Image with SDXL 0.9",
+  inputs=[gr.components.Textbox(lines=1,
                                 placeholder="Please enter text prompt here",
                                 label="Text",)],
   fn=generate_task,
@@ -26,7 +28,5 @@ webapp = gr.Interface(
 )
 
 # print(webapp)
-
-app = webapp()
-app.launch(server_name=str(SERVER_HOST),
+webapp.launch(server_name=str(SERVER_HOST),
            server_port=int(SERVER_PORT))
